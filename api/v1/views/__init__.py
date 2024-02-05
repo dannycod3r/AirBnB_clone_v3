bf838fdac2dba61988e2526a6b44bf5c13022760
root <root@BettysHp>
2024-02-06 01:31:08 +0300

    wht

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,7 @@
 """
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # import this after the app_views to prevent circular imports
 from api.v1.views.index import *
