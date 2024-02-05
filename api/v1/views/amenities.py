12f71bcc32e15645f0f5df61325331ed18e46394
root <root@BettysHp>
2024-02-05 23:34:25 +0300

    task8

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Modules docs
+"""
+
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from models import Amenity
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
+    amenities = Amenity.query.all()
+    amenities_list = [amenity.to_dict() for amenity in amenities]
+    return jsonify(amenities_list)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+    amenity = Amenity.query.get(amenity_id)
+    if amenity is None:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    amenity = Amenity.query.get(amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    return jsonify({})
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def create_amenity():
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    amenity = Amenity(**request.get_json())
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    amenity = Amenity.query.get(amenity_id)
+    if amenity is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict())
