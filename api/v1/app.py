ac1ffcc75b366fc35241cf6e74cf5e5dd2e908aa
dannycod3r <oseid7807@gmail.com>
2024-02-06 00:10:13 +0000

    Configue CORS for flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 
 host = os.environ.get('HBNB_API_HOST')
@@ -12,7 +13,9 @@ port = os.environ.get('HBNB_API_PORT')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+# configure CORS for flask
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
